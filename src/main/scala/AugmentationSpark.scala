748d6db3008eda796573fe48ed8f1a22314413f4
germangerken <germangerken@yandex.ru>
2023-02-16 16:29:14 +0300

    commit

--- src/main/scala/AugmentationSpark.scala
+++ src/main/scala/AugmentationSpark.scala
@@ -1,16 +1,18 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import AugmentationSpark_assist._
+import org.apache.spark.sql
+
 
 object AugmentationSpark {
   def main(args: Array[String]) = {
 
-  def augment(spark: SparkSession, inputDetailPath: String, outputPath: String,
-              timeIgnorance: Int = 5, breakPoint: Int = 1800): Unit = {
+  val augment = (spark: SparkSession, inputDetailPath: String, outputPath: String,
+              timeIgnorance: Int, breakPoint: Int) => {
 
 
     import spark.implicits._
-    
+
 
 
     val data = spark.read
@@ -19,7 +21,13 @@ object AugmentationSpark {
       .option("header",true)
       .load(inputDetailPath)
 
-    val df: DataFrame = data
+    val data_work = data.select(
+      $"ClientID".cast(sql.types.StringType),
+      $"user_path".cast(sql.types.StringType),
+      $"timeline".cast(sql.types.StringType)
+    )
+
+    val df: DataFrame = data_work
       .withColumn("time_seq", split(col("timeline"), "=>").cast("array<int>"))
       .withColumn("time_on_page", udf(timeOnPage _).apply(col("time_seq"), lit(timeIgnorance)))
       .withColumn("session_mask", udf(sessMask _).apply(col("time_on_page"), lit(breakPoint)))
@@ -43,10 +51,8 @@ object AugmentationSpark {
   }
 
 
-    val spark = SparkSession.builder().appName("Data Augmentation").getOrCreate()
-
-
-    augment(spark, "/Users/germangerken/Documents/test/test.csv", "/Users/germangerken/Documents/test/output", 5, 1800)
+    // val spark = SparkSession.builder().appName("Data Augmentation").getOrCreate()
+    // augment(spark, "/Users/germangerken/Documents/test/test.csv", "/Users/germangerken/Documents/test/output", 5, 1800)
 
   }
 }
